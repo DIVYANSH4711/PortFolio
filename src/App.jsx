8b5acf682b8d4271edf4b12c065524d58bfc1839
Divyansh Kashyap <duvyansh728@gmail.com>
2025-05-03 16:37:15 +0530

    add timeline of my education

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import SkillsSection from "./Components/SkillsSection/SkillsSection.jsx"
 import Programming from "./Components/Programming/Programming.jsx"
 import Home from "./Components/Home/Home.jsx"
 import Projects from "./Components/Projects/Projects.jsx"
+import Education from "./Components/Education/Education.jsx"
 
 
 function App() {
   return (
     <div className="w-full h-screen overflow-y-scroll scrollbar-hidden bg-black text-white scroll-smooth font-sans">
       {/* Navbar */}
-      <div className="fixed top-0 w-full h-14 flex justify-between items-center px-6 shadow-md z-20 border-b border-zinc-800 bg-black/90 backdrop-blur-sm">
+      {/* Navbar */}
+      <div className="hidden md:flex fixed top-0 w-full h-14 justify-between items-center px-6 shadow-md z-50 border-b border-zinc-800 bg-black/90 backdrop-blur-sm">
         {/* Indicator dots */}
         <div className="flex space-x-2">
           <div className="w-3 h-3 bg-red-500 rounded-full shadow-sm" />
@@ -18,36 +20,20 @@ function App() {
         </div>
         {/* Navbar Links */}
         <nav className="flex space-x-8 items-center text-sm cinzel-bold font-medium tracking-wide">
-          <a
-            href="#home"
-            className="px-2 py-1 hover:text-zinc-300 border-b-2 border-transparent hover:border-zinc-500 transition-all duration-300"
-          >
-            Home
-          </a>
-          <a
-            href="#skills"
-            className="px-2 py-1 hover:text-zinc-300 border-b-2 border-transparent hover:border-zinc-500 transition-all duration-300"
-          >
-            Skills
-          </a>
-          <a
-            href="#programming"
-            className="px-2 py-1 hover:text-zinc-300 border-b-2 border-transparent hover:border-zinc-500 transition-all duration-300"
-          >
-            Competitive Programming
-          </a>
-          <a
-            href="#projects"
-            className="px-2 py-1 hover:text-zinc-300 border-b-2 border-transparent hover:border-zinc-500 transition-all duration-300"
-          >
-            Projects
-          </a>
+          <a href="#home" className="px-2 py-1 hover:text-zinc-300 border-b-2 border-transparent hover:border-zinc-500 transition-all duration-300">Home</a>
+          <a href="#skills" className="px-2 py-1 hover:text-zinc-300 border-b-2 border-transparent hover:border-zinc-500 transition-all duration-300">Skills</a>
+          <a href="#programming" className="px-2 py-1 hover:text-zinc-300 border-b-2 border-transparent hover:border-zinc-500 transition-all duration-300">Competitive Programming</a>
+          <a href="#projects" className="px-2 py-1 hover:text-zinc-300 border-b-2 border-transparent hover:border-zinc-500 transition-all duration-300">Projects</a>
         </nav>
       </div>
+
 
       {/* Sections */}
       <section id="home" className="min-h-screen flex items-center justify-center border-b border-zinc-800">
         <Home />
+      </section>
+      <section id="Education" className="min-h-screen flex items-center justify-center border-b border-zinc-800">
+        <Education />
       </section>
       <section id="skills" className="min-h-screen flex items-center justify-center border-b border-zinc-800">
         <SkillsSection />
---
+++ src/Components/Education/Education.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import { Timeline } from "./ui/Timeline.jsx";
+
+
+export function Education() {
+   const data = [
+      {
+         title: "2018-19",
+         content: (
+            <div className="flex items-center justify-center">
+               <div className="bg-black/50 border border-zinc-800 rounded-lg overflow-hidden hover:border-zinc-600 transition-all duration-300 group">
+
+                  {/* Image Section with Overlay */}
+                  <div className="h-48 bg-white/95 overflow-hidden relative">
+                     <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent z-10" />
+                     <img
+                        src="/BSER.png"
+                        alt="BSER logo"
+                        className="w-full h-full object-contain object-center group-hover:scale-105 transition-all duration-500"
+                     />
+                  </div>
+
+                  {/* Text Content */}
+                  <div className="p-5">
+                     <h3 className="text-4xl cinzel-bold text-white mb-2">
+                        10th - Secondary
+                     </h3>
+                     <p className="text-xl cinzel-bold text-white mb-2">Percentage: 89.67%</p>
+                     <div  className="text-lg text-zinc-400 font-serif py-1 my-1">Adarsh Bal Vidhya Mandir, Aklera, Rajasthan</div>
+                     <div className="text-sm w-max font-serif px-2 py-1 my-1 bg-zinc-900 text-zinc-300 rounded-md border border-zinc-700">
+                        Board of Secondary Education, Rajasthan
+                     </div>
+                  </div>
+
+               </div>
+            </div>
+         ),
+      },
+      {
+         title: "2020-21",
+         content: (
+            <div className="flex items-center justify-center">
+               <div className="bg-black/50 border border-zinc-800 rounded-lg overflow-hidden hover:border-zinc-600 transition-all duration-300 group">
+
+                  {/* Image Section with Overlay */}
+                  <div className="h-48 bg-white/95 overflow-hidden relative">
+                     <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent z-10" />
+                     <img
+                        src="/BSER.png"
+                        alt="BSER logo"
+                        className="w-full h-full object-contain object-center group-hover:scale-105 transition-all duration-500"
+                     />
+                  </div>
+
+                  {/* Text Content */}
+                  <div className="p-5">
+                     <h3 className="text-4xl cinzel-bold text-white mb-2">
+                        12th - Senior Secondary
+                     </h3>
+                     <p className="text-xl cinzel-bold text-white mb-2">Percentage: 98.8%</p>
+                     <div  className="text-lg text-zinc-400 font-serif py-1 my-1">Adarsh Bal Vidhya Mandir, Aklera, Rajasthan</div>
+                     <div className="text-sm w-max font-serif px-2 py-1 my-1 bg-zinc-900 text-zinc-300 rounded-md border border-zinc-700">
+                        Board of Senior Secondary Education, Rajasthan
+                     </div>
+                  </div>
+
+               </div>
+            </div>
+         ),
+      },
+      {
+         title: "2022-Present",
+         content: (
+            <div className="flex items-end justify-center">
+               <div className="bg-black/50 border border-zinc-800 rounded-lg overflow-hidden hover:border-zinc-600 transition-all duration-300 group">
+
+                  {/* Image Section with Overlay */}
+                  <div className="h-48 bg-black overflow-hidden relative">
+                     <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent z-10" />
+                     <img
+                        src="/juet.png"
+                        alt="JUET logo"
+                        className="w-full h-full object-contain object-center group-hover:scale-105 transition-all duration-500"
+                     />
+                  </div>
+
+                  {/* Text Content */}
+                  <div className="p-5">
+                     <h3 className="text-4xl cinzel-bold text-white mb-2">
+                        Bachelor of Technology (B.Tech) 
+                     </h3>
+                     <p className="text-xl cinzel-bold text-white mb-2">Current CGPA: 7.8</p>
+                     <div  className="text-lg text-zinc-400 font-serif py-1 my-1">Jaypee University of Engineering & Technology</div>
+                     <div className="text-sm w-max font-serif px-2 py-1 my-1 bg-zinc-900 text-zinc-300 rounded-md border border-zinc-700">
+                        Computer Science and Engineering
+                     </div>
+                  </div>
+               </div>
+            </div>
+         ),
+      },
+   ];
+   return (
+      <div className="relative w-full overflow-clip">
+         <Timeline data={data} />
+      </div>
+   );
+}
+
+export default Education;
